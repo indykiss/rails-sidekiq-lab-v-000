245c55c38e9517cb56dbca711f5d7a886083f7d9
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-08 19:44:28 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,5 @@
 class SongsController < ApplicationController
   require 'csv'
-  include Sidekiq::Worker
-
-  def perform(songs_file)
-    CSV.foreach(songs_file, headers: true) do |song|
-      Song.create(name: song[0], artist_name: song[1])
-    end
-  end
 
   def index
     @songs = Song.all
